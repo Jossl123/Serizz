f3c89ef93d58e69f7cc693c5b33b50affb22de55
Josselin Gautier <josselin.gautier@etu.u-bordeaux.fr>
2024-01-09 10:27:52 +0100

    continue home front page

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -13,12 +13,14 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'app_default')]
     public function index(EntityManagerInterface $entityManager): Response
     {
+        //TODO return the good series 
         $seriesRepo = $entityManager
             ->getRepository(Series::class);
 
-        $series = $seriesRepo->findBy(array(), null, 3, 0);
+        $series = $seriesRepo->findBy(array(), null, 4, 0);
         return $this->render('default/index.html.twig', [
             "hall_of_fame" => $series,
+            "recently_seen" => $series
         ]);
     }
 }
