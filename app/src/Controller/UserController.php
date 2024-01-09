e1bebba20f8db672e2639318a9063de7d650e5f1
Josselin Gautier <josselin.gautier@etu.u-bordeaux.fr>
2024-01-09 12:06:00 +0100

    start embody thing

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -48,6 +48,14 @@ class UserController extends AbstractController
         ]);
     }
 
+    #[Route('/{id}', name: 'app_user_embody', methods: ['GET'])]
+    public function embody(User $user): Response
+    {
+        //TODO
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
