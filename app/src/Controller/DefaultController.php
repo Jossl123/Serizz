48f2b7f3a30d1d87f28bed1843d3dba1d6aabc12
Josselin Gautier <josselin.gautier@etu.u-bordeaux.fr>
2024-01-09 10:08:16 +0100

    start hall of fame

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Series;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $seriesRepo = $entityManager
+            ->getRepository(Series::class);
+
+        $series = $seriesRepo->findBy(array(), null, 3, 0);
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            "hall_of_fame" => $series,
         ]);
     }
 }
