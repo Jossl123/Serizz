fd627d9f699438b95f03b00b8469bc115128bd04
Josselin Gautier <josselin.gautier@etu.u-bordeaux.fr>
2024-01-10 08:48:48 +0100

    different rendered page if not connected

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Series;
-
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
@@ -18,9 +18,17 @@ class DefaultController extends AbstractController
             ->getRepository(Series::class);
 
         $series = $seriesRepo->findBy(array(), null, 4, 2);
+
+        if (!$this->isGranted('ROLE_USER')) {
+            return $this->render('default/showcase.html.twig', [
+                "hall_of_fame" => $series
+            ]);
+        }
+
         return $this->render('default/index.html.twig', [
             "hall_of_fame" => $series,
             "recently_seen" => $series
         ]);
     }
+
 }
