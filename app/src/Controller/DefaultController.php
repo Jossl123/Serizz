53ebdf845df590f9dc5047552d9140524662b28a
Josselin Gautier <josselin.gautier@etu.u-bordeaux.fr>
2024-01-10 09:41:49 +0100

    showcase page

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Series;
+use App\Entity\User;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 class DefaultController extends AbstractController
 {
@@ -16,12 +17,15 @@ class DefaultController extends AbstractController
         //TODO return the good series 
         $seriesRepo = $entityManager
             ->getRepository(Series::class);
-
+        $usersRepo = $entityManager
+            ->getRepository(User::class);
         $series = $seriesRepo->findBy(array(), null, 4, 2);
 
         if (!$this->isGranted('ROLE_USER')) {
             return $this->render('default/showcase.html.twig', [
-                "hall_of_fame" => $series
+                "hall_of_fame" => $series,
+                "user_nb" => $usersRepo->count([]),
+                "watched_episodes" => $usersRepo->count([]) * 10,//TODO
             ]);
         }
 
