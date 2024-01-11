fd81aacd39655efceaf03eb226cd2a24d0496e37
tbaradatfour <teo.baradat-fourane@etu.u-bordeaux.fr>
2024-01-11 15:34:25 +0100

    first version of fake-users-creating script

--- app/src/Command/TestCommand.php
+++ app/src/Command/TestCommand.php
@@ -6,6 +6,8 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use App\Factory\UserFactory;
 
 #[AsCommand(
     name: 'app:a-comm',
@@ -17,9 +19,20 @@ class TestCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        for ($i=0; $i < $input->getArgument('nb'); $i++) {
+            // TODO : replace that writeln w/ call to the creation of an user
+            $output->writeln('testy stuff : '.$i);
+        }
 
-        $output->writeln('testy stuff');
+        UserFactory::createMany($input->getArgument('nb'));
 
         return Command::SUCCESS;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('nb', InputArgument::REQUIRED, 'nb times')
+        ;
     }
 }
