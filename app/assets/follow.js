57c5e9dc7db14f1c431b5c9ade80167f5b5d1101
Josselin Gautier <josselin.gautier@etu.u-bordeaux.fr>
2024-01-11 13:52:01 +0100

    progress bar is working but not optimized

--- app/assets/follow.js
+++ app/assets/follow.js
@@ -28,19 +28,41 @@ function mark_as_seen(url_to_fetch, season_id) {
         if (!result.success) {
             return
         }
-        var ep = document.getElementById (`ep-${ep_id}`)
+        var progress_bar_serie = document.getElementById("progress_bar_serie");
+        var ep = document.getElementById(`ep-${ep_id}`)
+
         if (ep.classList.contains("nf-md-eye_off")) {
             ep.classList.remove("nf-md-eye_off")
             ep.classList.add("nf-fa-check")
+            var i = 0
+            var all_episodes = document.querySelectorAll("#seasons_episodes .nf")
+            while (i < all_episodes.length && all_episodes[i] != ep){
+                i++
+                all_episodes[i].classList.add("nf-fa-check")
+                all_episodes[i].classList.remove("nf-md-eye_off")
+            };
         } else {
             ep.classList.remove("nf-fa-check")
             ep.classList.add("nf-md-eye_off")
         }
 
-        var progress_bar_serie = document.getElementById("progress_bar_serie");
+        document.querySelectorAll('[id^="progress_bar_season_"]').forEach(season => {
+            var id_split = season.id.split("_")
+            var c_season_id = id_split[id_split.length-1]
+            var nb_checked_episodes_season = document.querySelectorAll('#ep-season-'+c_season_id+' .nf-fa-check').length;
+            var total_nb_episodes_season = document.querySelectorAll('#ep-season-'+c_season_id+' .nf').length;
+            var season_percent = parseInt(nb_checked_episodes_season/total_nb_episodes_season*100);
+            for (let i = season.classList.length - 1; i >= 0; i--) {
+                const className = season.classList[i];
+                if (className.startsWith('w-')) {
+                    season.classList.remove(className);
+                }
+                season.classList.add("w-["+season_percent+"%]")
+            }
+        });
+
         var nb_checked_episodes = document.querySelectorAll('#seasons_episodes .nf-fa-check').length;
         var total_nb_episodes = document.querySelectorAll('#seasons_episodes .nf-md-eye_off').length + nb_checked_episodes;
-        console.log(nb_checked_episodes, total_nb_episodes)
         var serie_percent = parseInt(nb_checked_episodes/total_nb_episodes*100)
         for (let i = progress_bar_serie.classList.length - 1; i >= 0; i--) {
             const className = progress_bar_serie.classList[i];
@@ -50,17 +72,6 @@ function mark_as_seen(url_to_fetch, season_id) {
             progress_bar_serie.classList.add("w-["+serie_percent+"%]")
         }
 
-        var progress_bar_season = document.getElementById("progress_bar_season_"+season_id);
-        var nb_checked_episodes_season = document.querySelectorAll('#ep-season-'+season_id+' .nf-fa-check').length;
-        var total_nb_episodes_season = document.querySelectorAll('#ep-season-'+season_id+' .nf').length;
-        var season_percent = parseInt(nb_checked_episodes_season/total_nb_episodes_season*100)
-        for (let i = progress_bar_season.classList.length - 1; i >= 0; i--) {
-            const className = progress_bar_season.classList[i];
-            if (className.startsWith('w-')) {
-                progress_bar_season.classList.remove(className);
-            }
-            progress_bar_season.classList.add("w-["+season_percent+"%]")
-        }
 
     }).catch(error => console.log('error', error));
 }
