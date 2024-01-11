e9d2d695e32c4cd8a494473b7271deb2cc499ba0
Callixte Muller <callixte.muller@gmail.com>
2024-01-11 09:50:03 +0100

    Updated security to auto login after registration

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Security\AppAuthAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -17,7 +19,9 @@ class RegistrationController extends AbstractController
     public function register(
         Request $request,
         UserPasswordHasherInterface $userPasswordHasher,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        UserAuthenticatorInterface $authenticator,
+        AppAuthAuthenticator $appAuthAuthenticator
     ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -36,7 +40,11 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_series_index');
+            return $authenticator->authenticateUser(
+                $user,
+                $appAuthAuthenticator,
+                $request
+            );
         }
 
         return $this->render('registration/register.html.twig', [
