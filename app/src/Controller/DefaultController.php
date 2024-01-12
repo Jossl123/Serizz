8ba65ef5f1504e426f268c83276ce05c8e109ce6
Lucas Fanner <lucas.fanner@etu-u.bordeaux.fr>
2024-01-12 14:52:04 +0100

    fix bugs rating

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Rating;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,6 +21,8 @@ class DefaultController extends AbstractController
             ->getRepository(Series::class);
         $usersRepo = $entityManager
             ->getRepository(User::class);
+        $ratingRepo = $entityManager
+            ->getRepository(Rating::class);
         $series = $seriesRepo->findBy(array(), null, 4, 2);
 
         if (!$this->isGranted('ROLE_USER')) {
