d990ca9ca204db83ef59e3234747d9a201d7068c
Callixte Muller <callixte.muller@gmail.com>
2024-01-15 11:44:10 +0100

    Implemented succesfully inserting new series into db

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Actor;
+use App\Entity\Country;
+use App\Entity\Episode;
+use App\Entity\Genre;
 use App\Entity\Rating;
+use App\Entity\Season;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Series;
 use App\Entity\User;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class DefaultController extends AbstractController
 {
+
+    public function __construct(private HttpClientInterface $client)
+    {
+
+    }
     #[Route('/', name: 'app_default')]
     public function index(EntityManagerInterface $entityManager): Response
     {
@@ -41,6 +58,143 @@ class DefaultController extends AbstractController
         return $this->render('default/index.html.twig', [
             "hall_of_fame" => $hallOfFameSeries,
             "recently_seen" => $followed_series
+        ]);
+    }
+
+    /**
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     */
+    #[Route('/add', name:'app_series_add')]
+    public function addSeries(Request $request, EntityManagerInterface $entityManager): Response {
+        $url = "http://www.omdbapi.com/?apikey=3c7a370d&type=series&";
+        $posterUrl = "http://img.omdbapi.com/?apikey=3c7a370d&";
+        $episodeUrl = "http://www.omdbapi.com/?apikey=3c7a370d&";
+        $title = $request->query->get('title', "");
+        $url .= "t=" . $title;
+        $series = $entityManager
+            ->getRepository(Series::class)
+            ->findBy(['title' => $title]);
+
+        $content = array();
+        $years = array();
+        $genres = array();
+        $actors = array();
+        $country = null;
+        $posterContent = null;
+
+        if (isset($_GET['title'])) {
+            $response = $this->client->request('GET', $url);
+            $statusCode = $response->getStatusCode();
+            $contentType = $response->getHeaders()['content-type'][0];
+            $content = $response->getContent();
+            $content = $response->toArray();
+
+            if($content['Response'] != "False") {
+                $years = explode("-", $content['Year']);
+                $genres = explode(", ", $content['Genre']);
+                $actors = explode(", ", $content['Actors']);
+                $country = $entityManager
+                    ->getRepository(Country::class)
+                    ->findBy(['name' => $content['Country']]);
+
+                $posterUrl .= "i=" . $content['imdbID'];
+                $posterResponse = $this->client->request('GET', $posterUrl);
+                $posterStatusCode = $posterResponse->getStatusCode();
+                if ($posterStatusCode == 200) {
+                    $posterContentType = $posterResponse->getHeaders()['content-type'][0];
+                    $posterContent = $posterResponse->getContent();
+                }
+            }
+
+        }
+
+        if (isset($_POST['add'])){
+            $series = new Series();
+            $series->setTitle($content['Title']);
+            dump($content['Title']);
+            $series->setPoster($posterContent);
+            $series->setYearStart(intval($years[0]));
+            if (isset($years[1])){
+                $series->setYearEnd(intval($years[1]));
+            }
+
+            for ($i = 0; $i < sizeof($genres); $i++){
+                $genre = new Genre();
+                $genre->setName($genres[$i]);
+                $genre->addSeries($series);
+                $series->addGenre($genre);
+                $entityManager->persist($genre);
+            }
+
+            $series->setPlot($content['Plot']);
+            $series->setImdb($content['imdbID']);
+            $series->setDirector($content['Director']);
+
+            for ($i = 0; $i < sizeof($actors); $i++){
+                $actor = new Actor();
+                $actor->setName($actors[$i]);
+                $actor->addSeries($series);
+                $series->addActor($actor);
+                $entityManager->persist($actor);
+            }
+
+            if ($country != null) {
+                $series->addCountry($country[0]);
+            } else {
+                $country = new Country();
+                $country->setName($content['Country']);
+                $country->addSeries($series);
+                $series->addCountry($country);
+                $entityManager->persist($country);
+            }
+
+            $series->setAwards($content['Awards']);
+            $series->setYoutubeTrailer("https://www.youtube.com/embed/o9CeEHUG1sU?controls=0&autoplay=1&mute=1&&showinfo=0autohide%3D2&playlist=o9CeEHUG1sU");
+            $entityManager->persist($series);
+
+            if(isset($content['totalSeasons'])){
+                for($i = 0; $i < $content['totalSeasons']; $i++){
+                    $episodeUrl .= "t=" . $content['Title'] . "&Season=" . ($i + 1);
+                    $episodeResponse = $this->client->request('GET', $episodeUrl);
+                    $episodeStatusCode = $episodeResponse->getStatusCode();
+                    $episodeContentType = $episodeResponse->getHeaders()['content-type'][0];
+                    $episodeContent = $episodeResponse->getContent();
+                    $episodeContent = $episodeResponse->toArray();
+
+                    $season = new Season();
+                    $season->setNumber($i);
+                    $season->setSeries($series);
+                    $series->addSeason($season);
+
+                    for($j = 0; $j < sizeof($episodeContent['Episodes']); $j++){
+                        $episode = new Episode();
+                        if(strlen($episodeContent['Episodes'][$j]['Title']) > 128) {
+                            $episode->setTitle(substr($episodeContent['Episodes'][$j]['Title'], 0, 128));
+                        } else {
+                            $episode->setTitle($episodeContent['Episodes'][$j]['Title']);
+                        }
+                        $episode->setNumber($episodeContent['Episodes'][$j]['Episode']);
+                        $episode->setImdb($episodeContent['Episodes'][$j]['imdbID']);
+                        $episode->setSeason($season);
+                        $season->addEpisode($episode);
+                        $entityManager->persist($episode);
+                    }
+                    $entityManager->persist($season);
+                }
+            }
+
+            $entityManager->flush();
+
+        }
+
+
+        return $this->render('default/add.html.twig', [
+            "content" => $content,
+            "series" => $series
         ]);
     }
 }
