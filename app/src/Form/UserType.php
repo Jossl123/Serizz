7c2a20bb8b516d7852d851c02a44130a40197438
Muller Callixte <callixte.muller@etu.u-bordeaux.fr>
2024-01-17 14:42:12 +0100

    Admin reset password

--- app/src/Form/UserType.php
+++ app/src/Form/UserType.php
@@ -9,8 +9,11 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
@@ -25,10 +28,32 @@ class UserType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('email')
+            ->add('email', null,[
+                'disabled' => true,
+                'attr' => [
+                    'class' => 'email-field'
+                ]
+            ])
             ->add('country', EntityType::class, [
                 'class' => Country::class,
                 'choice_label' => 'name',
+            ])
+            ->add('password', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
             ])
         ;
 
