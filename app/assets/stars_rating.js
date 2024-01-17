09ea92c287765b1c568ec2da0436b6b198d8ed24
Josselin Gautier <josselin.gautier@etu.u-bordeaux.fr>
2024-01-17 11:34:08 +0100

    add semi star rating

--- app/assets/stars_rating.js
+++ app/assets/stars_rating.js
@@ -1,8 +1,12 @@
+var rating = 0
+
 function rate_star_click(left,i){
     if(left){
+        rating = i*2+1
         rate_star_click_right_hide(i)
         rate_star_click_left_show(i)
     }else{
+        rating = i*2+2
         if (i<4)rate_star_click_left_hide(i+1)
         rate_star_click_right_show(i)
     }
@@ -26,4 +30,15 @@ function rate_star_click_left_hide(i){
 function rate_star_click_right_hide(i){
     document.getElementById(`rate-star-yellow-right-${i}`).style.display="none"
     if (i < 4)rate_star_click_left_hide(i+1)
+}
+
+function formSubmit(e) {
+    e.preventDefault();
+    const form = document.querySelector("form");
+    console.log(document.getElementById("series_rating_value").value)
+
+    document.getElementById("series_rating_value").value = rating;
+    console.log(document.getElementById("series_rating_value").value)
+    form.submit();
+    
 }
